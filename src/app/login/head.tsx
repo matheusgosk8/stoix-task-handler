e8a8ca11393e15470d31f860ec01a3cbf0de287c
imobi-matheus <mtsgosk8@hotmail.com>
2025-10-05 14:35:15 -0300

    metadata

---
+++ src/app/login/head.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'stoix-task-handler - login',
+  description: 'Página de login',
+  icons: {
+    icon: '/favicon.ico', 
+  },
+};
+
+export default function Head() {
+  return null; 
+}
--- src/config/db/database.ts
+++ src/config/db/database.ts
@@ -2,12 +2,8 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, 
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
-
-
 
 export const db = drizzle(pool);
